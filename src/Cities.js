89a521e08c617c58182b25a2d5afd9d40876819f
Chrissi <christina.surrey@gmx.de>
2022-09-25 20:18:54 +0000

    Update 4 files

---
+++ src/Cities.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Cities.css";
+
+export default function Cities() {
+    return ( <div className="Cities">
+    <ul>
+    <li> Sydney </li>
+     <li> Paris </li>
+      <li> Monreal </li>
+       <li> Lisbon </li>
+        <li> San Franciso </li>
+        </ul>
+        </div>
+    );     
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
+import Cities from "./Cities";
 
 import "./App.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-
-root.render(
-  <StrictMode>
+    
+  
+root.render (
     <div className="App">
       <h1>Challenge</h1>
       <h2>
-        Make european cities blue and north american cities red from a CSS
+       Move all cities into a new component
       </h2>
-      <li> Sydney </li>
-      <li className="Europe"> Paris </li>
-      <li className="NorthAmerican"> Montreal</li>
-      <li className="Europe"> Lisbon </li>
-      <li className="NorthAmerican"> San Francsico</li>
+    <Cities />
     </div>
-  </StrictMode>
-);
+)
